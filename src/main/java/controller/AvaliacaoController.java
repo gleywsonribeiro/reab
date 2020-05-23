ef542fac64cb3cf0db3084abf3cea3f40173aa31
Gleywson Ribeiro <gleywson@gmail.com>
2020-05-23 20:45:31 -0300

    Implementado o mecanismo de navegação múltiplas telas

--- src/main/java/controller/AvaliacaoController.java
+++ src/main/java/controller/AvaliacaoController.java
@@ -32,21 +32,21 @@ public class AvaliacaoController implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Paciente paciente;
-    private int pressaoArterial; //50-100
-    private int frequenciaCardiaca; //60-100
+    private Integer pressaoArterial; //50-100
+    private Integer frequenciaCardiaca; //60-100
 
-    private int FrequenciaRespiratoria; //12-20
+    private Integer FrequenciaRespiratoria; //12-20
     private boolean suporteVentilacao;
-    private int psv;  //<10
-    private int peep; //<8
-    private int fio2; // <0.6
+    private Integer psv;  //<10
+    private Integer peep; //<8
+    private Integer fio2; // <0.6
 
-    private int hemoglobina; //< 7
-    private int lactato; // > 2
-    private int plaquetas; //< 35
+    private Integer hemoglobina; //< 7
+    private Integer lactato; // > 2
+    private Integer plaquetas; //< 35
 
-    private int glasgow; // glasgow < 9
-    private int rass; // -2 ---- 2
+    private Integer glasgow; // glasgow < 9
+    private Integer rass; // -2 ---- 2
 
     private String resultado;
 
@@ -58,27 +58,27 @@ public class AvaliacaoController implements Serializable {
         this.paciente = paciente;
     }
 
-    public int getPressaoArterial() {
+    public Integer getPressaoArterial() {
         return pressaoArterial;
     }
 
-    public void setPressaoArterial(int pressaoArterial) {
+    public void setPressaoArterial(Integer pressaoArterial) {
         this.pressaoArterial = pressaoArterial;
     }
 
-    public int getFrequenciaCardiaca() {
+    public Integer getFrequenciaCardiaca() {
         return frequenciaCardiaca;
     }
 
-    public void setFrequenciaCardiaca(int frequenciaCardiaca) {
+    public void setFrequenciaCardiaca(Integer frequenciaCardiaca) {
         this.frequenciaCardiaca = frequenciaCardiaca;
     }
 
-    public int getFrequenciaRespiratoria() {
+    public Integer getFrequenciaRespiratoria() {
         return FrequenciaRespiratoria;
     }
 
-    public void setFrequenciaRespiratoria(int FrequenciaRespiratoria) {
+    public void setFrequenciaRespiratoria(Integer FrequenciaRespiratoria) {
         this.FrequenciaRespiratoria = FrequenciaRespiratoria;
     }
 
@@ -90,67 +90,67 @@ public class AvaliacaoController implements Serializable {
         this.suporteVentilacao = suporteVentilacao;
     }
 
-    public int getPsv() {
+    public Integer getPsv() {
         return psv;
     }
 
-    public void setPsv(int psv) {
+    public void setPsv(Integer psv) {
         this.psv = psv;
     }
 
-    public int getPeep() {
+    public Integer getPeep() {
         return peep;
     }
 
-    public void setPeep(int peep) {
+    public void setPeep(Integer peep) {
         this.peep = peep;
     }
 
-    public int getFio2() {
+    public Integer getFio2() {
         return fio2;
     }
 
-    public void setFio2(int fio2) {
+    public void setFio2(Integer fio2) {
         this.fio2 = fio2;
     }
 
-    public int getHemoglobina() {
+    public Integer getHemoglobina() {
         return hemoglobina;
     }
 
-    public void setHemoglobina(int hemoglobina) {
+    public void setHemoglobina(Integer hemoglobina) {
         this.hemoglobina = hemoglobina;
     }
 
-    public int getLactato() {
+    public Integer getLactato() {
         return lactato;
     }
 
-    public void setLactato(int lactato) {
+    public void setLactato(Integer lactato) {
         this.lactato = lactato;
     }
 
-    public int getPlaquetas() {
+    public Integer getPlaquetas() {
         return plaquetas;
     }
 
-    public void setPlaquetas(int plaquetas) {
+    public void setPlaquetas(Integer plaquetas) {
         this.plaquetas = plaquetas;
     }
 
-    public int getGlasgow() {
+    public Integer getGlasgow() {
         return glasgow;
     }
 
-    public void setGlasgow(int glasgow) {
+    public void setGlasgow(Integer glasgow) {
         this.glasgow = glasgow;
     }
 
-    public int getRass() {
+    public Integer getRass() {
         return rass;
     }
 
-    public void setRass(int rass) {
+    public void setRass(Integer rass) {
         this.rass = rass;
     }
 
@@ -195,4 +195,71 @@ public class AvaliacaoController implements Serializable {
         this.resultado = resultado;
     }
 
+    public String validarPressao() {
+        if (pressaoArterial > 50 && pressaoArterial < 100) {
+            return "frequenciaCardiaca?faces-redirect=true";
+        } else {
+            return "reprovado?faces-redirect=true";
+        }
+    }
+
+    public String validarFrequenciaCardiaca() {
+        if (frequenciaCardiaca > 60 && frequenciaCardiaca < 100) {
+            return "frequenciaRespiratoria?faces-redirect=true";
+        } else {
+            return "reprovado?faces-redirect=true";
+        }
+    }
+
+    public String validarFrequenciaRespiratoria() {
+        if (FrequenciaRespiratoria > 12 && FrequenciaRespiratoria < 20) {
+            if (suporteVentilacao) {
+                return "psv-ps?faces-redirect=true";
+            } else {
+                return "hemoglobina?faces-redirect=true";
+            }
+        } else {
+            return "reprovado?faces-redirect=true";
+        }
+    }
+
+    public String validarPsv() {
+        if (psv < 10) {
+            return "peep?faces-redirect=true";
+        } else {
+            return "reprovado?faces-redirect=true";
+        }
+    }
+
+    public String validarPeep() {
+        if (peep < 8) {
+            return "fio2";
+        } else {
+            return "reprovado?faces-redirect=true";
+        }
+    }
+
+    public String validarFio2() {
+        return (fio2 < 60) ? "hemoglobina?faces-redirect=true" : "reprovado?faces-redirect=true";
+    }
+
+    public String validarHemoglobina() {
+        return (hemoglobina < 7) ? "lactato?faces-redirect=true" : "reprovado?faces-redirect=true";
+    }
+
+    public String validarLactato() {
+        return (lactato > 2) ? "plaqueta?faces-redirect=true" : "reprovado?faces-redirect=true";
+    }
+
+    public String validarPlaquetas() {
+        return (plaquetas > 35) ? "glasgow?faces-redirect=true" : "reprovado?faces-redirect=true";
+    }
+
+    public String validarGlasgow() {
+        return (glasgow < 9) ? "rass?faces-redirect=true" : "reprovado?faces-redirect=true";
+    }
+
+    public String validarRass() {
+        return (rass >= -2 && rass <= 2) ? "aprovado?faces-redirect=true" : "reprovado?faces-redirect=true";
+    }
 }
