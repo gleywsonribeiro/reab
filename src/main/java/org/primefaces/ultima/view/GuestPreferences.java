57f068f6b3e2ad08e4041ab87f577e9d576098d0
gleywson Ribeiro <gleywson@gmail.com>
2021-02-11 17:52:55 -0300

    Ajuste no layout

--- src/main/java/org/primefaces/ultima/view/GuestPreferences.java
+++ src/main/java/org/primefaces/ultima/view/GuestPreferences.java
@@ -26,24 +26,24 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean
 @ApplicationScoped
 public class GuestPreferences implements Serializable {
-    
-    private Map<String,String> themeColors;
-    
-    private String theme = "teal";
-    
+
+    private Map<String, String> themeColors;
+
+    private String theme = "dark-blue";
+
     private String menuClass = null;
-    
+
     private String profileMode = "inline";
-    
+
     private String menuLayout = "static";
-    
+
     private boolean compact = true;
-    
+
     private boolean orientationRTL;
-    
+
     @PostConstruct
     public void init() {
-        themeColors = new HashMap<String,String>();
+        themeColors = new HashMap<String, String>();
         themeColors.put("indigo", "#3F51B5");
         themeColors.put("blue", "#03A9F4");
         themeColors.put("blue-grey", "#607D8B");
@@ -53,61 +53,64 @@ public class GuestPreferences implements Serializable {
         themeColors.put("purple-amber", "#673AB7");
         themeColors.put("purple-cyan", "#673AB7");
         themeColors.put("teal", "#009688");
+        setDarkMenu();
+        setCompact(true);
     }
-    
+
     public String getMenuClass() {
         return this.menuClass;
     }
-    
+
     public String getProfileMode() {
         return this.profileMode;
     }
 
-	public String getTheme() {		
-		return theme;
-	}
-    
-    public String getMenuLayout() {	
-        if(this.menuLayout.equals("static"))
-            return "menu-layout-static";
-        else if(this.menuLayout.equals("overlay"))
-            return "menu-layout-overlay";
-        else if(this.menuLayout.equals("horizontal"))
-            return "menu-layout-static menu-layout-horizontal";
-        else if(this.menuLayout.equals("slim"))
-            return "menu-layout-static layout-menu-slim";
-        else
-            return "menu-layout-static";
+    public String getTheme() {
+        return theme;
     }
 
-	public void setTheme(String theme) {
-		this.theme = theme;
-	}
-    
+    public String getMenuLayout() {
+        if (this.menuLayout.equals("static")) {
+            return "menu-layout-static";
+        } else if (this.menuLayout.equals("overlay")) {
+            return "menu-layout-overlay";
+        } else if (this.menuLayout.equals("horizontal")) {
+            return "menu-layout-static menu-layout-horizontal";
+        } else if (this.menuLayout.equals("slim")) {
+            return "menu-layout-static layout-menu-slim";
+        } else {
+            return "menu-layout-static";
+        }
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
     public void setLightMenu() {
         this.menuClass = null;
     }
-    
+
     public void setDarkMenu() {
         this.menuClass = "layout-menu-dark";
     }
-    
+
     public void setProfileMode(String profileMode) {
         this.profileMode = profileMode;
     }
-    
+
     public void setMenuLayout(String menuLayout) {
         this.menuLayout = menuLayout;
     }
-    
+
     public Map getThemeColors() {
         return this.themeColors;
     }
-    
+
     public void setCompact(boolean value) {
         this.compact = value;
     }
-    
+
     public boolean isCompact() {
         return this.compact;
     }
