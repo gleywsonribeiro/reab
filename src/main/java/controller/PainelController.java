5c2c722816f831c9392faf146fd9be21ef898864
Gleywson Ribeiro <gleywson@gmail.com>
2021-02-16 23:11:40 -0300

    Fazendo a criação do painel de leitos

---
+++ src/main/java/controller/PainelController.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import model.Paciente;
+import model.Setor;
+import model.dao.PacienteDao;
+import model.service.PacienteService;
+import model.service.SetorService;
+import util.exception.DBException;
+import util.exception.NegocioException;
+import util.jsf.JsfUtil;
+
+/**
+ *
+ * @author gleyw
+ */
+@ManagedBean
+@ViewScoped
+public class PainelController implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private SetorService setorService = new SetorService();
+    private Setor unidade = new Setor();
+
+    @PostConstruct
+    private void init() {
+        
+        String chave = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("id");
+
+        if (chave != null) {
+            Long id = Long.parseLong(chave);
+            unidade = setorService.buscarPorId(id);
+        }
+
+    }
+
+    public Setor getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(Setor unidade) {
+        this.unidade = unidade;
+    }
+
+    
+
+}
